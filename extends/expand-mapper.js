f2e68c0e7fa73fa4dffdcb299a12a2b2d43aa7fb
lr5420511 <783621006@qq.com>
2022-12-25 16:26:39 +0800

    完成三个最后插件

--- extends/expand-mapper.js
+++ extends/expand-mapper.js
@@ -7,7 +7,19 @@ module.exports = options => {
     }, options);
     return function expand_mapper(rt) {
         rt.presets.directives[operator] = (sval, tval, prop, host, fields, src, tar, index, ops) => {
-
+            const valid = Array.isArray(tval);
+            if(valid) {
+                const action = fields[name],
+                    [single, index] = Object.assign([false, tval.length], 
+                        action && action(sval, tval, src, tar)
+                    );
+                sval = [
+                    ...tval.slice(0, index),
+                    ...(Array.isArray(sval) && !single ? sval : [sval]),
+                    ...tval.slice(index) 
+                ];
+            }
+            return [sval, tval, prop, host];
         };
     };
 };
--- extends/increment-mapper.js
+++ extends/increment-mapper.js
@@ -7,7 +7,17 @@ module.exports = options => {
     }, options);
     return function increment_mapper(rt) {
         rt.presets.directives[operator] = (sval, tval, prop, host, fields, src, tar, index, ops) => {
-
+            let valid = 'object' === typeof sval || 'undefined' === typeof sval;
+            valid = valid && 'object' === typeof tval && tval;
+            if(valid) {
+                const regular = fields[name];
+                sval = Object.keys(sval || {}).reduce((res, key) => {
+                    const exclude = tval.hasOwnProperty(key) || (regular && regular.test(key));
+                    exclude || (res[key] = sval[key]);
+                    return res;
+                }, { ...tval });
+            }
+            return [sval, tval, prop, host];
         };
     };
 };
